d1a3d21a321bb5da2649e908f8e723788a7f94b7
Devin Pastoor <devin.pastoor@gmail.com>
2015-03-05 21:06:27 -0500

    save plots properly without exiting early

--- R/plot.R
+++ R/plot.R
@@ -8,21 +8,20 @@ plot <- function(x) {
 plot.qpc <- function(x) {
   rdata_quantiles <- x$r_pauc_quantiles
   sdata_quantiles <- x$s_pauc_quantiles
-  
+  plot_list <- list()
   library(ggplot2)
   for(i in seq_along(rdata_quantiles)) {
     rq1 <- rdata_quantiles[i]
     sq1 <- sdata_quantiles[i]
     rvalue <- data.frame(key = names(rq1[[1]]), rvalue = rq1[[1]])
-    
     summary <- dplyr::inner_join(gather(sq1[[1]], key, value, -REP), rvalue)
     p <- summary %>% ggplot(aes(x = value, group = key)) +geom_histogram(color = "black", fill="white") +
       facet_wrap(~key, scales="free") + PKPDmisc::base_theme() + geom_vline(aes(xintercept = rvalue), color = "red", size = 2) +
       xlab(names(rq1))
     print(p)
-    return(p)
+    plot_list[[i]] <- p
   }
-  
+  return(plot_list)
 }
 
 
